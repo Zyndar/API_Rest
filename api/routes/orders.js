65f2fad0f3692e45db61848bf102ace755ee4f51
Merk <thezyndar@gmail.com>
2019-10-27 14:12:52 +0100

    Working product database with GET POST DELETE

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,30 +1,36 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders fetched'
-    });
+  res.status(200).json({
+    message: 'Orders fetched',
+  });
 });
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Orders created'
-    });
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity,
+  };
+  res.status(201).json({
+    message: 'Orders created',
+    order: order,
+  });
 });
 
 router.get('/:orderID', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders details',
-        orderId: req.param.orderId
-    });
+  res.status(200).json({
+    message: 'Orders details',
+    orderId: req.param.orderId,
+  });
 });
 
 router.delete('/:orderID', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders deleted',
-        orderId: req.param.orderId
-    });
+  res.status(200).json({
+    message: 'Orders deleted',
+    orderId: req.param.orderId,
+  });
 });
 
 module.exports = router;
