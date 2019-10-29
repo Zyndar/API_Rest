b9939c6939c5144d65c0e85fbdc2b1a6b52b0f37
Merk <thezyndar@gmail.com>
2019-10-29 12:33:57 +0100

    Added Signup and Login

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,9 +7,28 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  Product.find().exec().then((docs) => {
-    console.log(docs);
-    res.status(200).json(docs);
+  Product.find().select('name price _id').exec().then((docs) => {
+    const response = {
+      count: docs.length,
+      products: docs.map((doc) => {
+        return {
+          name: doc.name,
+          price: doc.price,
+          _id: doc.id,
+          request: {
+            type: 'GET',
+            url: `http://localhost:3000/products/${doc._id}`,
+          },
+        };
+      }),
+    };
+    if (docs.length >= 0) {
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({
+        message: 'No entries found',
+      });
+    }
   }).catch((err) => {
     console.log(err);
     res.status(500).json({
@@ -27,8 +46,16 @@ router.post('/', (req, res, next) => {
   product.save().then((result) => {
     console.log(result);
     res.status(201).json({
-      message: 'Handling POST requests to /products',
-      createdProduct: result,
+      message: 'Created product.',
+      createdProduct: {
+        name: result.name,
+        price: result.price,
+        _id: result._id,
+        request: {
+          type: 'POST',
+          url: `http://localhost:3000/products/${result._id}`,
+        },
+      },
     });
   }).catch((err) => {
     console.log(err);
@@ -54,8 +81,19 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product!',
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps }).exec().then((result) => {
+    console.log(result);
+    res.status(200).json(result);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json({
+      error: err,
+    });
   });
 });
 
--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(`mongodb+srv://merk:${process.env.MONGO_ATLAS_PW}@rest-api-iewsq.mongodb.net/test?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not Found');
